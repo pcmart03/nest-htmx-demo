f4b878842943b6040307999b94f231225a24cb57
patrickmartin <pcmart03@gmail.com>
2023-12-18 16:45:24 -0500

    adds comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -31,7 +31,7 @@ export class AppController {
   }
 
   /**
-   * The contacts template is lazy loaded into the index view using htmx.
+   * The contacts template is lazy loaded into the index view using htmx's onload trigger.
    * Although the template lives in the view folder, it can be considered a partial.
    *
    * */
@@ -56,14 +56,24 @@ export class AppController {
     return res.render(type, { name });
   }
 
+  // --- nested routes ----
+
+  /**
+   * One of the downsides of htmx is nested routes can be more complicated.
+   * If the user refreshes the page, you need to return the entire view,
+   * not just the partial the nested route loads
+   * */
   @Get('/route1')
   route_one(@Req() req: Request, @Res() res) {
-    console.log(req.headers);
-    const showRoute = !req.headers['hx-request'];
-    const templateName = showRoute ? 'index' : 'route_1';
-    return res.render(templateName, { message: 'hello route 1', showRoute });
+    const isNotAJAX = !req.headers['hx-request'];
+
+    // index contains the entire view. route_1 is a partial of the nested content.
+    const templateName = isNotAJAX ? 'index' : 'route_1';
+    // Index has conditional that will show the route_1 content if showRoute is true.
+    return res.render(templateName, { message: 'hello route 1', showRoute: isNotAJAX });
   }
 
+  // TODO: add refresh fallback
   @Get('/route2')
   @Render('route_2')
   route_two(): { message: string } {
